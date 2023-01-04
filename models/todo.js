51706d0b6a7ea1dfca0a95b6254162d5886d3f06
ozamaahmed <93034776+ozamaahmed@users.noreply.github.com>
2023-01-04 15:02:10 +0530

    Add files via upload

--- models/todo.js
+++ models/todo.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
      * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
+     * And Also method is not part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
@@ -15,11 +15,12 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
+    static getTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
-    }
-    static getTodos() {
-      return this.findAll({order : [["id","ASC"]]});
     }
   }
   Todo.init(
